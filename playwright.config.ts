d0abaaa31cfd1268c597b364f45cc1461e48fc31
Hrqlv <henriquelopesvelozo@gmail.com>
2026-03-12 16:43:48 -0300

    test: Ajustes na config

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 3 : 0,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 2 : 0,
   reporter: [['list', { printSteps: true }], ['html'],  ['pwmochawesome', { outputJSON: true, outputFileName: 'result.json' }], ['allure-playwright', { outputFolder: 'allure-results' }]],
   
   use: {
